db6451c846543ce108d20c266af6a26ce108e608
BarbaraBoeters <barbaraboeters@hotmail.com>
2017-01-12 12:20:07 +0100

    Prototype

---
+++ barbaraboeters-project/AddPlantViewController.swift
@@ -0,0 +1,61 @@
+//
+//  AddPlantViewController.swift
+//  barbaraboeters-project
+//
+//  Created by Barbara Boeters on 12-01-17.
+//  Copyright © 2017 Barbara Boeters. All rights reserved.
+//
+
+import UIKit
+
+class AddPlantViewController: UIViewController {
+
+    @IBOutlet weak var image: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        image.layer.borderWidth = 1
+        image.layer.masksToBounds = false
+        image.layer.borderColor = UIColor.green.cgColor
+        image.layer.cornerRadius = image.frame.height/2
+        image.clipsToBounds = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func addPhoto(_ sender: Any) {
+        let alert = UIAlertController(title: "Photo",
+                                      message: "Add a photo",
+                                      preferredStyle: .alert)
+        
+        // TODO
+        let saveAction = UIAlertAction(title: "Save",
+                                       style: .default) { _ in
+                                        guard let textField = alert.textFields?.first,
+                                            let text = textField.text else { return }
+        }
+        let cancelAction = UIAlertAction(title: "Cancel",
+                                         style: .default)
+        alert.addTextField()
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
